38eac27c4d3aff15431b25f259441b505f0c9dec
Bruno <bru-a@hotmail.com>
2024-04-03 00:48:37 -0300

    Redirected Home page to /login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
+import { redirect } from "next/navigation";
 export default function Home() {
-  return (
-    <main>
-      <h1>Home</h1>
-      <h2>APAJAC - Sistema De Gerenciamento</h2>
-    </main>
-  );
+  redirect("/login");
 }
