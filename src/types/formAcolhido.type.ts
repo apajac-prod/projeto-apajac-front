bcb5daf1e8ba586e7e7de0d113b4e5d127fedc36
Bruno <bru-a@hotmail.com>
2024-04-27 20:33:24 -0300

    add apiAcolhido and formAcolhido types

--- src/types/formAcolhido.type.ts
+++ src/types/formAcolhido.type.ts
@@ -1,40 +1,42 @@
 export type Acolhido = {
     name: string | undefined;
-    birthdate: Date | undefined;
+    birthdate: Date | string | undefined;
     educationLevel: string | undefined;
     school: string | null | undefined;
-    schoolPhone: string | null | undefined;
+    schoolPhone: string | undefined | null;
     anyInstitutionRegister: string | undefined;
     whichInstitution: string | undefined;
     forwardedTo: string | undefined;
-    whoRecommended: string | null | undefined;
+    whoRecommended: string | undefined;
     informationProvidedBy: string | undefined;
 
     //Address
     postalCode: string | undefined;
-    address: string | undefined;
-    district: string | undefined;
-    city: string | undefined;
     fu: string | undefined;
+    city: string | undefined;
+    district: string | undefined;
+    address: string | undefined;
+    addressNumber: string | undefined;
+    complement: string | undefined | null;
   };
 
   export type Pais = {
     name: string | undefined;
     phones?: Array<{
-      value: string | null | undefined;
+      value?: string | undefined | null;
     }>,
     ocupation: string | undefined;
     placeOfWork: string | null | undefined;
-    salary: number | null | undefined;
-    employmentRelationship: string | undefined;
-    employmentRelationshipDesc: string | undefined;
+    salary: string | number | null | undefined;
+    employmentRelationship: string | null | undefined;
+    employmentRelationshipDesc: string | null | undefined;
   }
 
   export type Responsavel = {
     responsible: string | undefined;
-    responsibleWho: string | undefined;
+    responsibleName: string | null | undefined;
     phones?: Array<{
-      value: string | null | undefined;
+      value?: string | undefined | null;
     }>,
   }
 
