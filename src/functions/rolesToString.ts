71d503bd35ce20ce5b2aec443200ecb482f0867b
Bruno <bru-a@hotmail.com>
2024-05-27 17:20:15 -0300

    add Tailwind

---
+++ src/functions/rolesToString.ts
@@ -0,0 +1,23 @@
+const ROLES_TO_PERMISSOES = new Map([
+    ["admin", "Administrador"],
+    ["alterar_assistido", "Alterar assistido"],
+    ["consultar_assistido", "Consultar assistido"],
+    ["cadastrar_assistido", "Cadastrar assistido"],
+
+])
+
+export default function rolesToString(roles: Array<string>){
+    let rolesStr = "";
+
+    roles.forEach((role, index) => {
+        console.log("index:", index, "\n", "role:", role)
+        console.log("getRole:", ROLES_TO_PERMISSOES.get(role))
+        if (rolesStr == "") {
+            rolesStr = rolesStr + ROLES_TO_PERMISSOES.get(role);
+            return;
+        }
+        rolesStr = rolesStr + `, ${ROLES_TO_PERMISSOES.get(role)}`;
+    })
+    console.log("rolesStr:", rolesStr)
+    return rolesStr;
+}
---
+++ src/types/listAssistido.type.ts
@@ -0,0 +1,15 @@
+export type ListAssistido = {
+    id: string;
+    name: string;
+    age: number;
+    responsible: string;
+    status: boolean;
+}
+
+export type ListAssistidoApi = {
+    id: string;
+    nome: string;
+    idade: number;
+    responsavel: string;
+    statusAssistido: boolean;
+}
